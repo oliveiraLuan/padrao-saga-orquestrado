53fba7da3f2a20305d28f5ba92ddacc6c24a1b57
Luan de Oliveira <luan012012@hotmail.com>
2024-10-10 23:05:53 -0300

    feat: adicionado projeto product-validation-service

---
+++ product-validation-service/src/test/java/com/luandeoliveira/product_validation_service/ProductValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.product_validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
