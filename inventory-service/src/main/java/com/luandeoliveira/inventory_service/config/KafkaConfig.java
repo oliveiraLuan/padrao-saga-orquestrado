bdfe660cfe450ebdc0f3b9281d1e66f260c425b3
Luan de Oliveira <luan012012@hotmail.com>
2024-10-23 22:42:11 -0300

    feat: KafkaConfig para inventory-service

---
+++ inventory-service/src/main/java/com/luandeoliveira/inventory_service/config/KafkaConfig.java
@@ -0,0 +1,95 @@
+package com.luandeoliveira.inventory_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    @Value("${spring.data.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.data.consumer.group-id}")
+    private String groupId;
+    @Value("${spring.data.consumer.auto-offset-reset}")
+    private String autoOffReset;
+    @Value("${spring.kafka.topic.inventory-success}")
+    private String inventorySuccess;
+    @Value("${spring.kafka.topic.inventory-fail}")
+    private String inventoryFail;
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestrator;
+
+    private static final Integer PARTITIONS_QUANTITY = 1;
+    private static final Integer REPLICAS_QUANTITY = 1;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProps());
+    }
+
+    private Map<String, Object> consumerProps() {
+        var props = new HashMap<String, Object>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffReset);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerProps());
+    }
+
+    private Map<String, Object> producerProps() {
+        var props = new HashMap<String, Object>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    private NewTopic buildTopic(String name){
+        return TopicBuilder
+                .name(name)
+                .replicas(REPLICAS_QUANTITY)
+                .partitions(PARTITIONS_QUANTITY)
+                .build();
+    }
+
+    @Bean
+    private NewTopic orchestrator(){
+        return buildTopic(orchestrator);
+    }
+
+    @Bean
+    private NewTopic inventorySuccess(){
+        return buildTopic(inventorySuccess);
+    }
+    @Bean
+    private NewTopic inventoryFail(){
+        return buildTopic(inventoryFail);
+    }
+}
