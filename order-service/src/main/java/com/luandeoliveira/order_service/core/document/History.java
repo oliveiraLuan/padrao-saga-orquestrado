1269cf0147585c5eeca78624290cc6cd91d8e90c
Luan de Oliveira <luan012012@hotmail.com>
2024-11-09 19:13:21 -0300

    feat: criado History

---
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/document/History.java
@@ -0,0 +1,19 @@
+package com.luandeoliveira.order_service.core.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+    private String source;
+    private String status;
+    private String message;
+    private LocalDateTime createdAt;
+}
