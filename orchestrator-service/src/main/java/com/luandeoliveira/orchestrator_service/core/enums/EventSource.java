bbc865714e2c9e410a2cb2bc03da3a193332967a
Luan de Oliveira <luan012012@hotmail.com>
2024-11-10 23:44:05 -0300

    feat: criado enums EventSource e SagaStatus

---
+++ orchestrator-service/src/main/java/com/luandeoliveira/orchestrator_service/core/enums/EventSource.java
@@ -0,0 +1,8 @@
+package com.luandeoliveira.orchestrator_service.core.enums;
+
+public enum EventSource {
+    ORCHESTRATOR,
+    PRODUCT_VALIDATION_SERVICE,
+    PAYMENT_SERVICE,
+    INVENTORY_SERVICE
+}
---
+++ orchestrator-service/src/main/java/com/luandeoliveira/orchestrator_service/core/enums/SagaStatus.java
@@ -0,0 +1,7 @@
+package com.luandeoliveira.orchestrator_service.core.enums;
+
+public enum SagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
