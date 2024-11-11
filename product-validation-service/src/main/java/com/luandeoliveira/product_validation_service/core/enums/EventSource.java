197a6699b44010907693c4bb95e78dc8a90d332b
Luan de Oliveira <luan012012@hotmail.com>
2024-11-11 13:54:06 -0300

    feat: enums para product-validation-service

---
+++ product-validation-service/src/main/java/com/luandeoliveira/product_validation_service/core/enums/EventSource.java
@@ -0,0 +1,8 @@
+package com.luandeoliveira.product_validation_service.core.enums;
+
+public enum EventSource {
+    ORCHESTRATOR,
+    PRODUCT_VALIDATION_SERVICE,
+    PAYMENT_SERVICE,
+    INVENTORY_SERVICE
+}
---
+++ product-validation-service/src/main/java/com/luandeoliveira/product_validation_service/core/enums/SagaStatus.java
@@ -0,0 +1,7 @@
+package com.luandeoliveira.product_validation_service.core.enums;
+
+public enum SagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
