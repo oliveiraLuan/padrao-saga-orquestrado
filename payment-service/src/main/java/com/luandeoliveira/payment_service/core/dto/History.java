9c3f4242ddb83d07044395b781ee756e460686f2
Luan de Oliveira <luan012012@hotmail.com>
2024-11-11 13:56:31 -0300

    feat: dtos para payment-service

---
+++ payment-service/src/main/java/com/luandeoliveira/payment_service/core/dto/History.java
@@ -0,0 +1,21 @@
+package com.luandeoliveira.payment_service.core.dto;
+
+import com.luandeoliveira.payment_service.core.enums.EventSource;
+import com.luandeoliveira.payment_service.core.enums.SagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+    private EventSource source;
+    private SagaStatus status;
+    private String message;
+    private LocalDateTime createdAt;
+}
---
+++ payment-service/src/main/java/com/luandeoliveira/payment_service/core/dto/Product.java
@@ -0,0 +1,4 @@
+package com.luandeoliveira.payment_service.core.dto;
+
+public record Product(String code, Double unitValue) {
+}
