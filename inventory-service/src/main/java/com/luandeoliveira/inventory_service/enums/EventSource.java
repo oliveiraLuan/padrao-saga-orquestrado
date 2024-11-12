8ee5f796b56aa05b9311609feef554e6877997df
Luan de Oliveira <luan012012@hotmail.com>
2024-11-11 23:48:48 -0300

    feat: enums para inventory-service

---
+++ inventory-service/src/main/java/com/luandeoliveira/inventory_service/enums/EventSource.java
@@ -0,0 +1,8 @@
+package com.luandeoliveira.inventory_service.enums;
+
+public enum EventSource {
+    ORCHESTRATOR,
+    PRODUCT_VALIDATION_SERVICE,
+    PAYMENT_SERVICE,
+    INVENTORY_SERVICE
+}
---
+++ inventory-service/src/main/java/com/luandeoliveira/inventory_service/enums/SagaStatus.java
@@ -0,0 +1,7 @@
+package com.luandeoliveira.inventory_service.enums;
+
+public enum SagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
