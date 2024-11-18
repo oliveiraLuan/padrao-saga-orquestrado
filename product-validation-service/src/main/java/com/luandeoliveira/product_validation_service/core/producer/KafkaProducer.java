037d4e1a45dbb875dc7a7420a2bb1fe35bc76ddc
Luan de Oliveira <luan012012@hotmail.com>
2024-11-17 22:03:00 -0300

    feat: criado Kafka Producer

---
+++ product-validation-service/src/main/java/com/luandeoliveira/product_validation_service/core/producer/KafkaProducer.java
@@ -0,0 +1,26 @@
+package com.luandeoliveira.product_validation_service.core.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload){
+        try {
+            log.info("Enviando evento para o tópico {} com o payload {}", orchestratorTopic, payload);
+            kafkaTemplate.send(orchestratorTopic, payload);
+        }catch (Exception e){
+            log.error("Falha no envio do evento para o tópico {} com payload {}", orchestratorTopic, payload);
+        }
+    }
+}
