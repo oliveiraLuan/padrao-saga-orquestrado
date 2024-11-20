413ec9ab858c7b4aea460c707fb10a2a83bd16a6
Luan de Oliveira <luan012012@hotmail.com>
2024-11-19 22:58:08 -0300

    feat: criado EventConsumer

---
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/consumer/EventConsumer.java
@@ -0,0 +1,25 @@
+package com.luandeoliveira.order_service.core.consumer;
+
+import com.luandeoliveira.order_service.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class EventConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.notify-ending}"
+    )
+    public void consumeEventNotifyEnding(String payload){
+        log.info("Consumindo evento Notify-Ending com payload: {}", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
