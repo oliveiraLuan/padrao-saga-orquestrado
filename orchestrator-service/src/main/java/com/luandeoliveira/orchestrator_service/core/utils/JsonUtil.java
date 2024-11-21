e1227137885847f7b0bdfa6a8e9497d58001adb4
Luan de Oliveira <luan012012@hotmail.com>
2024-11-20 21:39:47 -0300

    feat: criado JsonUtil para orchestrator-service

---
+++ orchestrator-service/src/main/java/com/luandeoliveira/orchestrator_service/core/utils/JsonUtil.java
@@ -0,0 +1,32 @@
+package com.luandeoliveira.orchestrator_service.core.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luandeoliveira.orchestrator_service.core.dto.Event;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object){
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            log.error("Não foi possível converter o objeto para json{}", e.getMessage());
+            return "";
+        }
+    }
+
+    public Event toEvent(String json){
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception e){
+            log.error("Não foi possível converter o json para Event.class");
+            return null;
+        }
+    }
+}
