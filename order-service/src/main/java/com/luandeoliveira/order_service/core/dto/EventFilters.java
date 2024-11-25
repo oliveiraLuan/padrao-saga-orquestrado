c70f8be9e45aa11d76540305120c06ad6fb246ef
Luan de Oliveira <luan012012@hotmail.com>
2024-11-24 21:35:23 -0300

    feat: criado EventFilters para order-service

---
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/dto/EventFilters.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.order_service.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventFilters {
+    private String orderId;
+    private String transactionId;
+}
