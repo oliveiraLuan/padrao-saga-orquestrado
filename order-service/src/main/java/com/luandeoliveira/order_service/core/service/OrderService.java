6231f1bbeaea776063238abc1b59fdf077875bf5
Luan de Oliveira <luan012012@hotmail.com>
2024-11-28 23:48:35 -0300

    feat: criado OrderService

---
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/service/OrderService.java
@@ -0,0 +1,54 @@
+package com.luandeoliveira.order_service.core.service;
+
+import com.luandeoliveira.order_service.core.document.Event;
+import com.luandeoliveira.order_service.core.document.Order;
+import com.luandeoliveira.order_service.core.dto.OrderRequest;
+import com.luandeoliveira.order_service.core.producer.SagaProducer;
+import com.luandeoliveira.order_service.core.repository.EventRepository;
+import com.luandeoliveira.order_service.core.repository.OrderRepository;
+import com.luandeoliveira.order_service.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final EventRepository eventRepository;
+    private final SagaProducer sagaProducer;
+    private final JsonUtil jsonUtil;
+
+    private String TRANSACATION_ID_PATTERN = "%s_%s";
+
+    public Order createOrder(OrderRequest orderRequest){
+        Order order = Order
+                .builder()
+                .products(orderRequest.getProducts())
+                .createdAt(LocalDateTime.now())
+                .transactionId(String.format(TRANSACATION_ID_PATTERN, Instant.now().toEpochMilli(), UUID.randomUUID()))
+                .build();
+
+        orderRepository.save(order);
+        sagaProducer.sendEvent(jsonUtil.toJson(createPayload(order)));
+        return order;
+    }
+
+    public Event createPayload(Order order){
+        Event event = Event
+                .builder()
+                .orderId(order.getId())
+                .createdAt(LocalDateTime.now())
+                .payload(order)
+                .transactionId(order.getTransactionId())
+                .build();
+        eventRepository.save(event);
+        return event;
+    }
+
+}
