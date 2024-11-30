a789a9ff2ccba74b6726a81ea5f94fcb0736a11e
Luan de Oliveira <luan012012@hotmail.com>
2024-11-29 23:29:09 -0300

    feat: criado método notifyEnding

--- order-service/src/main/java/com/luandeoliveira/order_service/core/consumer/EventConsumer.java
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package com.luandeoliveira.order_service.core.consumer;
 
+import com.luandeoliveira.order_service.core.service.EventService;
 import com.luandeoliveira.order_service.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
 
     private final JsonUtil jsonUtil;
+    private final EventService eventService;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
--- order-service/src/main/java/com/luandeoliveira/order_service/core/service/EventService.java
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/service/EventService.java
@@ -3,13 +3,24 @@ package com.luandeoliveira.order_service.core.service;
 import com.luandeoliveira.order_service.core.document.Event;
 import com.luandeoliveira.order_service.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventService {
 
     private EventRepository eventRepository;
+
+    public void notifyEnding(Event event){
+        event.setOrderId(event.getPayload().getId());
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+        log.info("Pedido com {} saga notificado! Transaction id: {}", event.getPayload(), event.getTransactionId());
+    }
 
     public Event save(Event event){
         return eventRepository.save(event);
