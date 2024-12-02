1f8c566bfb101c18703af57f380eabd0b2caf33d
Luan de Oliveira <luan012012@hotmail.com>
2024-12-01 22:41:54 -0300

    feat: criado métodos de find no EventRepository

--- order-service/src/main/java/com/luandeoliveira/order_service/core/repository/EventRepository.java
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/repository/EventRepository.java
@@ -3,5 +3,11 @@ package com.luandeoliveira.order_service.core.repository;
 import com.luandeoliveira.order_service.core.document.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findAllByCreatedAtDesc();
+    Optional<Event> findTop1ByOrderIdOrderByCreatedAtDesc(String orderId);
+    Optional<Event> findTop1ByTransactionIdOrderByCreatedAtDesc(String transactionId);
 }
