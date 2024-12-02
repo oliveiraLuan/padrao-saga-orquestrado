dcdf03f86717f76bac6af606f42bec4be4a767bf
Luan de Oliveira <luan012012@hotmail.com>
2024-12-01 22:42:18 -0300

    feat: criado métodos de busca na EventService

--- order-service/src/main/java/com/luandeoliveira/order_service/core/service/EventService.java
+++ order-service/src/main/java/com/luandeoliveira/order_service/core/service/EventService.java
@@ -1,12 +1,16 @@
 package com.luandeoliveira.order_service.core.service;
 
 import com.luandeoliveira.order_service.core.document.Event;
+import com.luandeoliveira.order_service.core.dto.EventFilters;
 import com.luandeoliveira.order_service.core.repository.EventRepository;
+import com.luandeoliveira.order_service.exceptions.ValidationException;
+import io.micrometer.common.util.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -24,5 +28,33 @@ public class EventService {
 
     public Event save(Event event){
         return eventRepository.save(event);
+    }
+
+    public List<Event> findAll(){
+        return eventRepository.findAllByCreatedAtDesc();
+    }
+
+    public Event findByFilters(EventFilters filters) {
+        if (isFiltersEmpty(filters))
+            throw new ValidationException("Filters must be informed");
+
+        if (StringUtils.isNotBlank(filters.getOrderId()))
+            return findByOrderId(filters.getOrderId());
+
+        return findByTransactionId(filters.getTransactionId());
+    }
+
+    public Event findByOrderId(String orderId){
+        return eventRepository.findTop1ByOrderIdOrderByCreatedAtDesc(orderId)
+                .orElseThrow(() -> new ValidationException("Event with this orderId not found."));
+    }
+
+    public Event findByTransactionId(String transactionId){
+        return eventRepository.findTop1ByTransactionIdOrderByCreatedAtDesc(transactionId)
+                .orElseThrow(() -> new ValidationException("Event with this transactionId not found."));
+    }
+
+    public Boolean isFiltersEmpty(EventFilters filters){
+        return StringUtils.isBlank(filters.getOrderId()) && StringUtils.isBlank(filters.getTransactionId());
     }
 }
