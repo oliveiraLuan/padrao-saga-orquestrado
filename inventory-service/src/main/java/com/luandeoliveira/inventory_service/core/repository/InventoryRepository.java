df8f8d18f36a2b23852b7201d0c51b1fac6f7afc
Luan de Oliveira <luan012012@hotmail.com>
2024-12-21 22:58:44 -0300

    feat: criado InventoryRepository

---
+++ inventory-service/src/main/java/com/luandeoliveira/inventory_service/core/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.luandeoliveira.inventory_service.core.repository;
+
+import com.luandeoliveira.inventory_service.core.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
+    Optional<Inventory> findByProductCode(String productCode);
+}
