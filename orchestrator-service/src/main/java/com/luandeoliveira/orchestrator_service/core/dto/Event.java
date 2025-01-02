3223d88adbb7b3a1f916ed5d405070d59ecf3aa1
Luan de Oliveira <luan012012@hotmail.com>
2025-01-01 23:42:42 -0300

    feat: adicionado addHistory ao Event

--- orchestrator-service/src/main/java/com/luandeoliveira/orchestrator_service/core/dto/Event.java
+++ orchestrator-service/src/main/java/com/luandeoliveira/orchestrator_service/core/dto/Event.java
@@ -8,7 +8,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -23,4 +26,11 @@ public class Event {
     private LocalDateTime createdAt;
     private SagaStatus status;
     private EventSource source;
+
+    public void addHistory(History history){
+        if(isEmpty(eventHistory)){
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
